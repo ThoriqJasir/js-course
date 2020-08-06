68838bcbd2e9730818c4c99f8cb26095ea40033f
ThoriqJasir <68496689+ThoriqJasir@users.noreply.github.com>
2020-08-06 09:12:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//Variable data type
+
+/*
+var firstName = 'Thoriq';
+console.log(firstName);
+
+var lastName = 'Jasir';
+var age = 16;
+
+var fullAge = true;
+console.log(fullAge)
+
+var job = 'Student'
+console.log(job);
+*/
+
+
+//Variable mutation and type coercion
